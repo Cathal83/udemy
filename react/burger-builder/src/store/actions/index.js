d617b3b2ce55cc28aeed06273ddb8db74bebd8cc
Cathal O'Brien <csalem83@gmail.com>
2019-07-17 14:58:48 +0100

    Redux Saga
    Added Redux Saga, an alternative to Thunk

--- react/burger-builder/src/store/actions/index.js
+++ react/burger-builder/src/store/actions/index.js
@@ -1,8 +1,30 @@
 export {
   addIngredient,
   removeIngredient,
-  initIngredients
+  initIngredients,
+  setIngredients,
+  fetchIngredientsFailed
 } from "./burgerBuilder";
-export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrdersFail,
+  fetchOrdersSuccess,
+  fetchOrdersStart
+} from "./order";
 
-export { auth, logout, setAuthRedirectPath, authCheckState } from "./auth";
+export {
+  auth,
+  logout,
+  setAuthRedirectPath,
+  authCheckState,
+  logoutSucceed,
+  authStart,
+  authSuccess,
+  authFail,
+  checkAuthTimeout
+} from "./auth";
