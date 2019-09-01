a497d18707734c28848942066dae88fe62300458
Cathal O'Brien <csalem83@gmail.com>
2019-09-01 20:19:00 +0100

    End Section 3

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <>
         <NavBar />
-        <EventDashboard />
-      </div>
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </>
     );
   }
 }
