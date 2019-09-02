6c21c7e8411c39143a54fea994c9eec52c224798
Cathal O'Brien <csalem83@gmail.com>
2019-09-02 10:53:56 +0100

    Section 5 Complete

--- revents/src/features/event/EventList/EventListItem.jsx
+++ revents/src/features/event/EventList/EventListItem.jsx
@@ -4,7 +4,7 @@ import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
   render() {
-    const { event } = this.props;
+    const { event, selectEvent, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -26,7 +26,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attandees &&
+            {event.attendees &&
               event.attendees.map(attendee => (
                 <EventListAttendee key={attendee.id} attendee={attendee} />
               ))}
@@ -34,7 +34,20 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <Button
+            onClick={() => deleteEvent(event.id)}
+            as="a"
+            color="red"
+            floated="right"
+            content="Delete"
+          />
+          <Button
+            onClick={() => selectEvent(event)}
+            as="a"
+            color="teal"
+            floated="right"
+            content="View"
+          />
         </Segment>
       </Segment.Group>
     );
