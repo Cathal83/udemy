7aac271d1af405eb116effed2aae68aa2b4bb469
Cathal O'Brien <csalem83@gmail.com>
2019-09-02 10:00:18 +0100

    Section 4 Complete

---
+++ revents/src/features/event/EventForm/EventForm.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+
+class EventForm extends Component {
+  state = {
+    title: "",
+    date: "",
+    city: "",
+    venue: "",
+    hostedBy: ""
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    this.props.createEvent(this.state);
+  };
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    const { cancelFormOpen } = this.props;
+    const { title, date, city, venue, hostedBy } = this.state;
+
+    return (
+      <div>
+        <Segment>
+          <Form onSubmit={this.handleFormSubmit} autoComplete="off">
+            <Form.Field>
+              <label>Event Title</label>
+              <input
+                name="title"
+                value={title}
+                placeholder="Event Title"
+                onChange={this.handleInputChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Event Date</label>
+              <input
+                name="date"
+                type="date"
+                value={date}
+                placeholder="Event Date"
+                onChange={this.handleInputChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>City</label>
+              <input
+                name="city"
+                value={city}
+                placeholder="City event is taking place"
+                onChange={this.handleInputChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Venue</label>
+              <input
+                name="venue"
+                value={venue}
+                placeholder="Enter the Venue of the event"
+                onChange={this.handleInputChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Hosted By</label>
+              <input
+                name="hostedBy"
+                value={hostedBy}
+                placeholder="Enter the name of person hosting"
+                onChange={this.handleInputChange}
+              />
+            </Form.Field>
+            <Button positive type="submit">
+              Submit
+            </Button>
+            <Button type="button" onClick={cancelFormOpen}>
+              Cancel
+            </Button>
+          </Form>
+        </Segment>
+      </div>
+    );
+  }
+}
+export default EventForm;
