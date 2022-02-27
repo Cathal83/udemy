4deafd100285a797328f76ee8f2818942e0961dc
CathalCG <57371731+CathalCG@users.noreply.github.com>
2022-02-27 14:45:37 +0000

    Base files

---
+++ react2022/feedback-app/src/components/FeedbackList.jsx
@@ -0,0 +1,57 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useContext } from "react";
+
+// import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
+import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
+
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext);
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
+  // return (
+  //   <div className="feedback-list">
+  //     <AnimatePresence>
+  //       {feedback.map((item) => (
+  //         <motion.div
+  //           key={item.id}
+  //           initial={{ opacity: 0 }}
+  //           animate={{ opacity: 1 }}
+  //           exit={{ opacity: 0 }}
+  //         >
+  //           <FeedbackItem
+  //             key={item.id}
+  //             item={item}
+  //             handleDelete={handleDelete}
+  //           />{" "}
+  //         </motion.div>
+  //       ))}
+  //     </AnimatePresence>
+  //   </div>
+  // );
+}
+
+// FeedbackList.propType = {
+//   feedback: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       text: PropTypes.string.isRequired,
+//       rating: PropTypes.number.isRequired,
+//     })
+//   ),
+// };
+
+export default FeedbackList;
